b905928c012db03ce7602f8f1bcc466c610ed122
lakshna <lakshnajk@gmail.com>
2016-10-28 22:03:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,51 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick = function() {
+
+    var request=new XMLHttpRequest();
+
+
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+
+            if(request.status===200) {
+
+
+             }
+        }
+    };
+
+    request.open('GET','http://amiteshjk.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+
+var submit=document.getElementById('submit_btn');
+
+submit.onclick=function(){
+
+     var request=new XMLHttpRequest();
+
+
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+
+            if(request.status===200) {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML=list;
+
+
+             }
+        }
+    };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+
+    request.open('GET','http://amiteshjk.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+};
